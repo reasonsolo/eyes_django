419dbe420e0db851446fdb3d8358accc4380a7ec
newday <newday@naiba.com>
2019-01-24 17:54:39 +0800

    update auth

--- eyes1000/urls.py
+++ eyes1000/urls.py
@@ -21,6 +21,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pet/', include('pet.urls')),
+    path('wx_auth/', include('wx_auth.urls')),
 ]
 
 if settings.DEBUG:
