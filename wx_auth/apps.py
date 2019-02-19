99895d2aa0f7e7a61872f8767a2f1212f6977864
newday <newday@naiba.com>
2019-02-19 09:17:07 +0800

    add sms api

--- wx_auth/apps.py
+++ wx_auth/apps.py
@@ -11,6 +11,5 @@ class WxAuthConfig(AppConfig):
     WXAPP_ID = environ.get('WXAPP_ID', 'wxc9005a5b8f18e404')
     WXAPP_SECRET = environ.get('WXAPP_SECRET', '88dd56352d2712c0946519f10b226a09')
     # SMS
-    SMSAPP_ID = environ.get('SMSAPP_ID', '1400186429')
-    SMSAPP_KEY = environ.get('SMSAPP_KEY', '91034299f8e5d0021b38b92ac5f750cf')
-
+    SMSAPP_ID = environ.get('SMSAPP_ID', '1400185060')
+    SMSAPP_KEY = environ.get('SMSAPP_KEY', 'eb61ec1022c9ccec0d86d28206a5c061')
