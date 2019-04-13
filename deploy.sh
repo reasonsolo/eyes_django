6c8454cc5591f2fc2d446e7c1ca7b6a34242a0eb
eyes_django <newday@naiba.com>
2019-04-13 23:11:10 +0800

    add new crontab

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 crontab -r
-echo "* * 10 * * /root/letsencrypt/certbot-auto renew" > timercron
+echo "21 0 * * 1 /root/letsencrypt/certbot-auto renew" > timercron
 echo "*/10 * * * * python3 /data/eyes_django/wx_auth/app_token.py" >> timercron
+echo "*/10 * * * * python3 /data/eyes_django/clean_qrcode.py" >> timercron
 crontab ./timercron
